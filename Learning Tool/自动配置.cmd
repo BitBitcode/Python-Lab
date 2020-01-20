0a57a36b0057ea9b2ffbd45afc52bcb57becb323
BitBitcode <1378028451@qq.com>
2020-01-20 19:46:08 +0800

    删除了无用的文件

---
+++ Learning Tool/自动配置.cmd
@@ -0,0 +1,20 @@
+@echo off
+:: 切换字符编码为：UTF-8
+chcp 65001
+title 自动配置网页代码运行助手
+
+if EXIST learning.py (
+    python learning.py
+    echo 网页代码运行助手已启动！
+) else (
+    echo 当前路径没有文件，重新引导至默认位置。。。
+    cd C:\Users\smile\Documents\Visual Studio Code\Python_Code\Learning Tool
+    echo 路径已引导
+    if EXIST learning.py (
+        python learning.py
+        echo 默认位置运行成功！
+    ) else (
+        echo 默认位置没有文件!
+    )
+)
+pause
